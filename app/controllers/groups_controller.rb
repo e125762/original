8f5fd6785dda747f3d5e64df12f1089dc69e741a
keito.gibo <e125762@ie.u-ryukyu.ac.jp>
2018-06-06 05:45:20 +0000

    group show edit destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class GroupsController < ApplicationController
   before_action :require_user_logged_in
-
+  before_action :correct_user, only: [:destroy]
   def index
     @groups = Group.all.order('created_at DESC').page(params[:page])
   end
@@ -21,18 +21,42 @@ class GroupsController < ApplicationController
     @group = current_user.groups.build
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      flash[:success] = '登録が完了しました'
+      redirect_to groups_url
+    else
+      flash.now[:danger] = '登録に失敗しました'
+      render :edit
+    end
   end
 
   def destroy
+    @group.destroy
+    flash[:success] = 'メッセージを削除しました'
+    redirect_to groups_url
   end
 
   private
 
   def group_params
     params.require(:group).permit(:title, :description)
+  end
+
+  def correct_user
+    @group = current_user.groups.find_by(id: params[:id])
+    unless @group
+      redirect_to root_url
+    end
   end
 end
