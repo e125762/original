1a4af32a6a6767149c86200a40866531cc488996
keito.gibo <e125762@ie.u-ryukyu.ac.jp>
2018-06-07 01:56:56 +0000

    show group member

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,8 @@
 class GroupsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
+  before_action :require_group_join, only: [:show]
+
   def index
     @groups = Group.all.order('created_at DESC').page(params[:page])
   end
@@ -23,6 +25,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @group_users = @group.joind_users
   end
 
   def edit
@@ -56,6 +59,13 @@ class GroupsController < ApplicationController
   def correct_user
     @group = current_user.groups.find_by(id: params[:id])
     unless @group
+      redirect_to root_url
+    end
+  end
+
+  def require_group_join
+    group = Group.find(params[:id])
+    unless current_user.joind?(group)
       redirect_to root_url
     end
   end
