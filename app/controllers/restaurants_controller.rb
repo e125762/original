4f336e6afb9ba2f7b4782f15e532de664e41a2b5
keito.gibo <e125762@ie.u-ryukyu.ac.jp>
2018-06-11 13:30:32 +0000

    add link

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
     @areas = Area.all
+#    @group = params[:group_id]
     if params[:q]
     if params[:q][:area].present? && params[:search].present?
       @restaurants = Area.find(params[:q][:area]).restaurants.search(params[:search])
